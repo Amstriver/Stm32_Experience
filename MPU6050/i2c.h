10a3580d42cd37c826773521c84ef0ac16a8768c
WenJie <139532126+RoseRrin@users.noreply.github.com>
2024-06-18 17:28:48 +0800

    Add files via upload

---
+++ MPU6050/i2c.h
@@ -0,0 +1,63 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    i2c.h
+  * @brief   This file contains all the function prototypes for
+  *          the i2c.c file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __I2C_H__
+#define __I2C_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+#define IIC_WR	0		/* Ð´¿ØÖÆbit */
+#define IIC_RD	1		/* ¶Á¿ØÖÆbit */
+
+void IIC_Start(void);
+void IIC_Stop(void);
+void IIC_Send_Byte(uint8_t _ucByte);
+uint8_t IIC_Read_Byte(uint8_t ack);
+uint8_t IIC_Wait_Ack(void);
+void IIC_Ack(void);
+void IIC_NAck(void);
+uint8_t IIC_CheckDevice(uint8_t _Address);
+void IIC_GPIO_Init(void);
+/* USER CODE END Includes */
+
+extern I2C_HandleTypeDef hi2c2;
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void MX_I2C2_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __I2C_H__ */
+
